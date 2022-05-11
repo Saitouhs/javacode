b3c3b43a6fb652cba6d87bff6e94da4a582e4a82
saitou <saitouhirosaki@outlook.com>
2022-05-11 15:11:08 +0800

    1511

--- zrl6.java
+++ zrl6.java
@@ -2,8 +2,8 @@ class zrl6
 {
     public static void main(String []args)
     {
-        byte ch =100;
-        System.out.printf("%x\t%o\t%c\n",ch,ch,ch);
+        byte ch =0101;
+        System.out.printf("%x\t%d\t%o\t%c\n",ch,ch,ch,ch);
        
     }
 }
